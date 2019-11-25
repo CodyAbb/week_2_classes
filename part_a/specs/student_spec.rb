0d0c3ec4d6df855ab740c9157cd00ceed4896ea0
Cody Abbott <cody.abbott16@gmail.com>
2019-11-25 14:34:42 +0000

    setup student class with getter and setter methods

--- part_a/specs/student_spec.rb
+++ part_a/specs/student_spec.rb
@@ -3,9 +3,18 @@ require('minitest/reporters')
 require_relative('../student.rb')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-class TestBankAccount < MiniTest::Test
+class TestStudent < MiniTest::Test
 
+  def setup
+    @test_student = Student.new("Cody", "E36")
+  end
 
+  def test_check_student
+    assert_equal("Cody", @test_student.name)
+  end
 
+  def test_check_cohort
+    assert_equal("E36", @test_student.cohort)
+  end
 
 end
---
+++ part_a/student.rb
@@ -0,0 +1,18 @@
+class Student
+
+  attr_accessor :name, :cohort
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  # def get_student_name()
+  #   return @name
+  # end
+  #
+  # def get_cohort_name()
+  #     return @cohort
+  # end
+
+end
