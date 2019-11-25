dd1b0695db2c5d52f1adca8c6c87749d4d3770ed
Cody Abbott <cody.abbott16@gmail.com>
2019-11-25 15:06:34 +0000

    created part b setup

---
+++ part_b/specs/sports_team_spec.rb
@@ -0,0 +1,11 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../sport_team.rb')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestSportTeam < MiniTest::Test
+
+
+
+
+end
