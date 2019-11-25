9d118f2260af25e4c295eb333181d84f483bd749
Cody Abbott <cody.abbott16@gmail.com>
2019-11-25 15:41:08 +0000

    fixed issue with sport team call, and refactored attributes

--- part_b/specs/sports_team_spec.rb
+++ part_b/specs/sports_team_spec.rb
@@ -1,11 +1,23 @@
 require('minitest/autorun')
 require('minitest/reporters')
-require_relative('../sport_team.rb')
+require_relative('../sports_team.rb')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSportTeam < MiniTest::Test
 
+  def setup
+    @test_team = SportsTeam.new('Edinburgh Edisons', ['John', 'Jacob', 'Jane', 'Julia'], 'Matteo')
+  end
 
+  def test_get_team_name
+    assert_equal('Edinburgh Edisons', @test_team.team_name)
+  end
 
+  def test_get_players
+    assert_equal(['John', 'Jacob', 'Jane', 'Julia'], @test_team.players)
+  end
 
+  def test_get_coach
+    assert_equal('Matteo', @test_team.coach)
+  end
 end
