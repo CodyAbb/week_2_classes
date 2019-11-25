f2e6919ca0f9c831f0fd1d22fbd8421800f62b9d
Cody Abbott <cody.abbott16@gmail.com>
2019-11-25 16:03:54 +0000

    finished part_b, with refactors

--- part_b/specs/sports_team_spec.rb
+++ part_b/specs/sports_team_spec.rb
@@ -6,7 +6,7 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class TestSportTeam < MiniTest::Test
 
   def setup
-    @test_team = SportsTeam.new('Edinburgh Edisons', ['John', 'Jacob', 'Jane', 'Julia'], 'Matteo')
+    @test_team = SportsTeam.new('Edinburgh Edisons', ['John', 'Jacob', 'Jane', 'Julia'], 'Matteo', 0)
   end
 
   def test_get_team_name
@@ -20,4 +20,24 @@ class TestSportTeam < MiniTest::Test
   def test_get_coach
     assert_equal('Matteo', @test_team.coach)
   end
+
+  def test_change_coach
+    assert_equal("Alex", @test_team.coach = "Alex")
+  end
+
+  def test_add_player_to_team
+    @test_team.add_player("James")
+    assert_equal(5, @test_team.players.length)
+  end
+
+  def test_check_player_in_team
+    assert_equal(true, @test_team.check_player_exists("Jacob"))
+    assert_equal(false, @test_team.check_player_exists("Mark"))
+  end
+
+  def test_point_for_win
+    @test_team.points_for_game("Win")
+    assert_equal(3, @test_team.points)
+  end
+
 end
--- part_b/sports_team.rb
+++ part_b/sports_team.rb
@@ -1,12 +1,13 @@
 class SportsTeam
 
-  attr_accessor :coach
-  attr_reader :team_name, :players
+  attr_accessor :coach, :players, :points
+  attr_reader :team_name
 
-  def initialize (team_name, players, coach)
+  def initialize (team_name, players, coach, points)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = points
   end
 
   # def get_team_name
@@ -20,4 +21,24 @@ class SportsTeam
   # def get_coach
   #   return @coach
   # end
+
+  def add_player(player_name)
+    @players << player_name
+  end
+
+  def check_player_exists(player_name)
+    for player in @players
+      if player == player_name
+        return true
+      end
+    end
+    return false
+  end
+
+  def points_for_game(game_results)
+    if game_results == "Win"
+      @points += 3
+    end
+  end
+
 end
